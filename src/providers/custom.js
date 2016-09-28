9f5d42df35dd55f4de2105e881c6b5e1856a5046
Vojtech Soban <vojtech.soban@morosystems.cz>
2016-09-28 23:51:56 +0200

    Add moro restaurants

--- src/providers/custom.js
+++ src/providers/custom.js
@@ -1,55 +1,20 @@
-var $ = require('cheerio')
+var $ = require('cheerio');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 var request = require('request-promise');
 
-Date.prototype.getWeek = function () {
-    var onejan = new Date(this.getFullYear(), 0, 1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
-}
-
-function isEmptyOrSpaces(str) {
-    return str === null || str.match(/^ *$/) !== null;
-}
-
-function today(td) {
-    var d = new Date();
-    return td.getDate() == d.getDate() && td.getMonth() == d.getMonth() && td.getFullYear() == d.getFullYear();
-}
-
-var avion = async(function () {
+var zelenaKocka = async(function () {
     var data = await(request.get({
-        url: "http://avion58.cz/"
+        url: "http://www.zelenakocka.cz"
     }));
 
     var res = [];
 
     var parsedHTML = $.load(data);
-    parsedHTML('strong.price').map(function (i, food) {
-        var name = $(this).prev().text();
+    parsedHTML('#dnesni-menu br').map(function () {
+        var name = this.nextSibling.nodeValue;
 
         if (name) {
-            res.push({
-                "name": name,
-                "price": $(this).text()
-            });
-        }
-    });
-
-    return res;
-});
-
-var motoburger = async(function () {
-    var data = await(request.get({
-        url: "http://www.motoburger.cz/"
-    }));
-
-    var res = [];
-
-    var parsedHTML = $.load(data);
-    parsedHTML('tr').map(function (i, food) {
-        var name = $(this).next().text();
-        if (name && !name.startsWith("*ROZVOZ")) {
             res.push({
                 "name": name
             });
@@ -57,23 +22,22 @@ var motoburger = async(function () {
     });
 
     return res;
-})
+});
 
-var puzzle = async(function () {
+var lightOfIndia = async(function () {
     var data = await(request.get({
-        url: "http://www.puzzlesalads.cz/denni-nabidka"
+        url: "http://www.lightofindia.cz/"
     }));
 
     var res = [];
 
     var parsedHTML = $.load(data);
-    parsedHTML('span.price').map(function (i, food) {
-        var name = $(this).prev().text();
 
-        if (!isEmptyOrSpaces(name)) {
+    parsedHTML('strong').map(function () {
+        var name = $(this).text();
+        if (name && name.startsWith("Týdenní menu pro dnešní den")) {
             res.push({
-                "name": name,
-                "price": $(this).text().replace(/(\r\n|\n|\r)/gm, " ").replace(/\s\s+/g, ' ')
+                "name": $(this).parent().text()
             });
         }
     });
@@ -81,68 +45,30 @@ var puzzle = async(function () {
     return res;
 });
 
-var eurest = async(function () {
-    var weekNumber = (new Date()).getWeek();
-
-    var data = await(request.get({
-        url: "http://www.unasrestaurace.cz/menulist.php?locale=cs&week=" + weekNumber
-    }));
-
-    var res = [];
-
-    var j = JSON.parse(data);
-    var todaysDate = new Date();
-
-    for (var i = 0; i < j.Items.length; ++i) {
-
-        var re = /-?\d+/;
-        var m = re.exec(j.Items[i].Date);
-        var d = new Date(parseInt(m[0]));
-
-        var isToday = today(d);
-
-        if (isToday) {
-            res.push({
-                "name": j.Items[i].MealName
-            });
-        }
-    }
-
-    return res;
-});
-
 module.exports = {
     handles: function (restaurant) {
-        return restaurant == "avion" || restaurant == "motoburger" || restaurant == "puzzle" || restaurant == "eurest";
+        return restaurant == "zelenaKocka" || restaurant == "lightOfIndia";
     },
 
     restaurants: function () {
-        return ["avion", "motoburger", "puzzle", "eurest"]
+        return ["zelenaKocka", "lightOfIndia"]
     },
 
     get: async(function (restaurant) {
         switch (restaurant) {
-            case "avion":
-                return await(avion());
-            case "eurest":
-                return await(eurest());
-            case "puzzle":
-                return await(puzzle());
-            case "motoburger":
-                return await(motoburger());
+            case "zelenaKocka":
+                return await(zelenaKocka());
+            case "lightOfIndia":
+                return await(lightOfIndia());
         }
     }),
 
     name: function (restaurant) {
         switch (restaurant) {
-            case "avion":
-                return "AVION 58";
-            case "eurest":
-                return "EUREST";
-            case "puzzle":
-                return "PUZZLE SALADS";
-            case "motoburger":
-                return "MOTOBURGER";
+            case "zelenaKocka":
+                return "Zelená kočka";
+            case "lightOfIndia":
+                return "Light of India";
         }
     }
 };
